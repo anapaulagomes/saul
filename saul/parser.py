949c19fd8a732739f596b7e3de1b5d6a56a3e418
Marcos Brizeno <marcos.uece.comp@gmail.com>
2017-04-21 14:21:29 -0300

    Use git log --raw formating and ignore merge commits

--- saul/parser.py
+++ saul/parser.py
@@ -1,4 +1,12 @@
 class LogParser(object):
 
     def commit(self, commit_log):
-        return [changed_file.strip() for changed_file in commit_log.split('\n')[1:]]
+        changed_files_log = self.__extract_changed_files_log(commit_log.split('\n'))
+        return [self.__extract_file_name(changed_file_log) for changed_file_log in changed_files_log]
+
+    def __extract_file_name(self, changed_file_log):
+        return changed_file_log.split(' ')[-1]
+
+    def __extract_changed_files_log(self, commit_log_lines):
+        if (commit_log_lines[1].startswith('Merge:')): return []
+        return commit_log_lines[6:]
