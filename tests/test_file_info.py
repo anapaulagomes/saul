da31695938581d65c332767f10d2a34506ed92f3
Ana Paula Gomes <apgomes88@gmail.com>
2017-05-18 18:41:59 -0300

    Verify if count changes after add related files

--- tests/test_file_info.py
+++ tests/test_file_info.py
@@ -16,3 +16,11 @@ def test_add_change_when_existent_related_file_is_added():
     file_info.add_change([related_file])
 
     assert file_info.related_files.get(related_file) == 2
+
+def test_count_changes_after_add_related_files():
+    file_info = FileInfo('home/file_path', ['path/to/some_file.py'])
+
+    related_file = 'path/to/some_file.py'
+    file_info.add_change([related_file])
+
+    assert file_info.changes == 2
