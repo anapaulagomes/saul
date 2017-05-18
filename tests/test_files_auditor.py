f5ad552a0263fe6fdea07ec0a88b779baf301a0f
Ana Paula Gomes <apgomes88@gmail.com>
2017-05-18 18:13:03 -0300

    Add htmlcov from test coverage plugin

--- tests/test_files_auditor.py
+++ tests/test_files_auditor.py
@@ -1,39 +1,39 @@
 from saul.file_info_repository import *
 
 def test_create_file_with_change_and_related_files():
-    auditor = FileInfoRepository()
+    repository = FileInfoRepository()
 
     a_file = 'path/to/file.py'
     related_files = ['path/to/another_file.py', 'path/to/some_file.py']
-    auditor.add_or_update(a_file, related_files)
+    repository.add_or_update(a_file, related_files)
 
-    assert auditor.find(a_file).changes == 1
-    assert len(auditor.find(a_file).related_files) == 2
+    assert repository.find(a_file).changes == 1
+    assert len(repository.find(a_file).related_files) == 2
 
 def test_add_related_files_to_existing_file():
-    auditor = FileInfoRepository()
+    repository = FileInfoRepository()
 
     a_file = 'path/to/file.py'
     related_files = ['path/to/another_file.py', 'path/to/some_file.py']
-    auditor.add_or_update(a_file, related_files)
+    repository.add_or_update(a_file, related_files)
 
     more_related_files = ['path/to/yet_another_file']
-    auditor.add_or_update(a_file, more_related_files)
+    repository.add_or_update(a_file, more_related_files)
 
-    assert auditor.find(a_file).changes == 2
-    assert len(auditor.find(a_file).related_files) == 3
+    assert repository.find(a_file).changes == 2
+    assert len(repository.find(a_file).related_files) == 3
 
 def test_increase_changes_when_file_is_already_related():
-    auditor = FileInfoRepository()
+    repository = FileInfoRepository()
 
     a_file = 'path/to/file.py'
     related_file = 'path/to/some_file.py'
     related_files = ['path/to/another_file.py',related_file]
-    auditor.add_or_update(a_file, related_files)
+    repository.add_or_update(a_file, related_files)
 
     more_related_files = [related_file]
-    auditor.add_or_update(a_file, more_related_files)
+    repository.add_or_update(a_file, more_related_files)
 
-    assert auditor.find(a_file).changes == 2
-    assert len(auditor.find(a_file).related_files) == 2
-    assert auditor.find(a_file).related_files[related_file] == 2
+    assert repository.find(a_file).changes == 2
+    assert len(repository.find(a_file).related_files) == 2
+    assert repository.find(a_file).related_files[related_file] == 2
