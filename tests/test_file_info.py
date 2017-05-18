a8b9cdde83e1574213831e2ce86cc8f839d1188e
Ana Paula Gomes <apgomes88@gmail.com>
2017-05-18 18:38:26 -0300

    Verify if add count changes right when a related file is added

--- tests/test_file_info.py
+++ tests/test_file_info.py
@@ -8,3 +8,11 @@ def test_add_change_when_new_related_file_is_added():
     file_info.add_change([related_file])
 
     assert file_info.related_files.get(related_file)
+
+def test_add_change_when_existent_related_file_is_added():
+    file_info = FileInfo('home/file_path', ['path/to/some_file.py'])
+
+    related_file = 'path/to/some_file.py'
+    file_info.add_change([related_file])
+
+    assert file_info.related_files.get(related_file) == 2
