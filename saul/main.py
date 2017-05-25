a11c865b709a2d0800ae1ddd475046ab47c0dcf8
Marcos Brizeno <marcos.uece.comp@gmail.com>
2017-05-25 19:05:01 -0300

    Handle file not found exception

--- saul/main.py
+++ saul/main.py
@@ -2,15 +2,16 @@ from saul.parser import LogParser
 import argparse
 
 
-diretory = 'saul.log'
 argument_parser = argparse.ArgumentParser()
 argument_parser.add_argument('-f', '--file', dest='log_file', default='saul.log', help='File from git log')
 
 captured_args = argument_parser.parse_args()
 
-
 parser = LogParser()
-repository_files = parser.log(open(captured_args.log_file).read())
+try:
+    repository_files = parser.log(open(captured_args.log_file).read())
+    for _, file_info in repository_files.items():
+        print(file_info)
+except FileNotFoundError:
+    print("Could not find {}! Use 'git log --raw --no-merges > {}' to generate a log file.".format(captured_args.log_file, captured_args.log_file))
 
-for _, file_info in repository_files.items():
-    print(file_info)
