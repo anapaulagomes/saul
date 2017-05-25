53dc1fbc6ce3003928e8cf3b56f0c1d408fbbfd1
Marcos Brizeno <marcos.uece.comp@gmail.com>
2017-05-25 18:58:43 -0300

    Add argument parsing to read file

--- saul/main.py
+++ saul/main.py
@@ -1,10 +1,16 @@
 from saul.parser import LogParser
+import argparse
 
 
 diretory = 'saul.log'
+argument_parser = argparse.ArgumentParser()
+argument_parser.add_argument('-f', '--file', dest='log_file', default='saul.log', help='File from git log')
+
+captured_args = argument_parser.parse_args()
+
 
 parser = LogParser()
-repository_files = parser.log(open(diretory).read())
+repository_files = parser.log(open(captured_args.log_file).read())
 
 for _, file_info in repository_files.items():
     print(file_info)
